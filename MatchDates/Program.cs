d6021018aee105accb5c291cc94245c422994e92
ivanPeshterskii <peshterskiivan@gmail.com>
2025-07-17 03:09:45 +0300

    Initial commit

---
+++ MatchDates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MatchDates;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        string pattern = @"\b(?<day>\d{2})([./-])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+
+        Regex regex = new Regex(pattern);
+
+        MatchCollection matchCollection = regex.Matches(input);
+
+        foreach (Match match in matchCollection)
+        {
+            string date = match.Groups["day"].Value;
+            string month = match.Groups["month"].Value;
+            string year = match.Groups["year"].Value;
+
+            Console.WriteLine($"Day: {date}, Month: {month}, Year: {year}");
+        }
+
+    }
+}
---
+++ MatchPhoneNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"(?<=\s|^)\+359([ -])2\1\d{3}\1\d{4}\b";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            var validMatches = matches
+                .Cast<Match>()
+                .Select(m => m.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", validMatches));
+        }
+    }
+}
